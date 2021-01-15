b2cf6879adf13fd8c1b152fdc5c9aca071ec3e72
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-15 14:10:32 -0700

    login ui finished

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -80,29 +80,38 @@ function Login(props) {
         vw-100 min-vh-100 money-background flex flex-row justify-center 
         ">
             <div className="
-            flex flex-column w-80 items-center mv3 br2 
-            flex-column-ns
-            flex-row-m min-vh-50-m
-            flex-row-l min-vh-50-l
+            flex flex-column w-90 items-center mv3 br2 
+            flex-column-ns w-80-ns
+            flex-column-m w-80-m
+            flex-row-l w-80-l
             ">
                 <div className="
                 w-100 flex white ph2 pt2 pb5 flex-column bg-money-color-see-through tc
-                h-100-m
-                h-100-l
+                vh-75-l justify-center-l
                 ">
                     <h1>Money Manager</h1>
                     <div>Money is a good servant but a terrible master. Get in control of your finances and sign up today!</div>
                 </div>
                 <div className="
                 w-100 flex flex-column pv3 bg-dark-color ph3
+                vh-75-l 
                 ">
-                    <div className="white tc">Try Money Manage with a demo account!</div>
-                    <div className="flex flex-row">
-                        <div className="w4 h3 mv1 mh1 ph1 pv2 bg-money-color white tc br1">Personal</div>
-                        <div className="w4 h3 mv1 mh1 ph1 pv2 bg-money-color white tc br1">Household Member</div>
-                        <div className="w4 h3 mv1 mh1 ph1 pv2 bg-money-color white tc br1">Household Owner</div> 
+                    <div className="
+                    white 
+                    mb2-l mt4-l
+                    ">Try Money Manage with a demo account!
+                    </div>
+                    <div className="flex flex-row mv3">
+                        <div className="w4 h3 flex justify-center items-center mv1 mh1 ph1 pv2 bg-money-color white br1 tc pointer grow">Personal</div>
+                        <div className="w4 h3 flex justify-center items-center mv1 mh1 ph1 pv2 bg-money-color white br1 tc pointer grow">Household Member</div>
+                        <div className="w4 h3 flex justify-center items-center mv1 mh1 ph1 pv2 bg-money-color white br1 tc pointer grow">Household Owner</div> 
                     </div>
                     <hr className="w-90"/>
+                    <div className="
+                    white mv2
+                    mv3-l
+                    ">Log in to your Money Manage Account
+                    </div>
                     <div className="flex flex-column pv2">
                         <label className="pl1 white">Email:</label>
                         <input type="text" onChange={(event) => setLoginEmail(event.target.value)}  className="br2"/>
@@ -113,16 +122,20 @@ function Login(props) {
                     </div>
                     {
                         (loginErrors.loginError) ? 
-                        <div>
+                        <div className="white">
                             {loginErrors.loginErrorMessage}
                         </div>
                         : ""
                     }
-                    <div onClick={() => sendLogin()} className="width-60-px ph1 pv2 mv3 bg-money-color br2 tc white">Log in</div>
-                    <div className="money-color mt3 mb1">Reset Password?</div>
+                    <div className="flex flex-row items-center">
+                        <div className="white pr2 mt2 mb1">Remember Log in?</div>
+                        <input type="checkbox" className="flex mt2"></input>
+                    </div>
+                    <div onClick={() => sendLogin()} className="width-60-px ph1 pv2 mv3 bg-money-color br2 tc white pointer grow">Log in</div>   
+                    <div className="money-color mt3 mb1 pointer underline-hover">Reset Password?</div>
                     <div className="flex flex-row mb3">
                         <div className="white">Don't have an account?</div>
-                        <div onClick={() => setRoute("signup")} className="pl1 money-color">Sign up!</div>
+                        <div onClick={() => setRoute("signup")} className="pl1 money-color pointer underline-hover">Sign up!</div>
                     </div>
                 </div>
             </div>
