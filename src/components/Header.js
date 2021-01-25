7d3a2d2c6d29616d66e19b7439818894f9b1ed9c
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-25 15:54:38 -0700

    working on mobile navigation

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Header(props) {
+
+    return(
+        <div className="h3 w100 flex flex-row bg-custom-lighter-gray bb-thin-gray">
+            <div className="w-50">
+                <svg 
+                    className="w2 h2 fixed pa0 ma3 white money-color 
+                    display-none-m
+                    display-none-l
+                    " 
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+                </path>
+                </svg>
+            </div>
+            <div className="w-50 flex flex-row items-center">
+                <div className="flex flex-row">
+                    <div className="money-color">Money</div>
+                    <div className="custom-gray">Manager</div>
+                </div>
+
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Header from "./Header.js";
 
 function Home(props) {
     const { setRoute, user } = props;
 
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="bg-custom-darker-gray">
+            <Header></Header>
+            <h1 className="vh-100">Home Page</h1>
              <div>{user.firstName}</div>
              <button onClick={() => setRoute("login")}>Login</button>
              <button onClick={() => setRoute("signup")}>Sign up</button>
