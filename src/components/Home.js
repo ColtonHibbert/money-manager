4701c4125ae736f5deb37e80663914a42e339f6e
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-25 16:50:17 -0700

    working on mobile menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,18 +20,21 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 function Home(props) {
-    const { setRoute, user } = props;
+    const { setRoute, user, navigation } = props;
 
     return (
         <div className="bg-custom-darker-gray">
             <Header {...props}></Header>
-            <h1 className="vh-100">Home Page</h1>
-             <div>{user.firstName}</div>
-             <button onClick={() => setRoute("login")}>Login</button>
-             <button onClick={() => setRoute("signup")}>Sign up</button>
-             <button onClick={() => setRoute("accounts")}>Accounts</button>
-             <button onClick={() => setRoute("transactions")}>Transactions</button>
-             <button onClick={() => setRoute("logout")}>Logout</button>
+            {
+                // 
+            }
+            
+            <div>{user.firstName}</div>
+            <button onClick={() => setRoute("login")}>Login</button>
+            <button onClick={() => setRoute("signup")}>Sign up</button>
+            <button onClick={() => setRoute("accounts")}>Accounts</button>
+            <button onClick={() => setRoute("transactions")}>Transactions</button>
+            <button onClick={() => setRoute("logout")}>Logout</button>
         </div>
     )
 }
