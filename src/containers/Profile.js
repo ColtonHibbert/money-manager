faeb724f7f9fd252a70f0e4a1d824262de586dda
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-01-28 11:43:44 -0700

    change mobile menu behavior

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -3,8 +3,14 @@ import React from "react";
 function Profile(props) {
     return (
         <div>
+            <div className="h2000">
             Profile
+            </div>
+            <div id="item2">
+                item2
+            </div>
         </div>
+        
     )
 }
 
