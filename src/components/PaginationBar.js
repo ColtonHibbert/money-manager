ba0f621434ef7e87684b8b24052b3da1a0991189
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-02-03 08:32:23 -0700

    pagination

--- src/components/PaginationBar.js
+++ src/components/PaginationBar.js
@@ -17,7 +17,65 @@ function PaginationBar(props) {
             ">
                 <div className="w-30 custom-gray">{`Showing ${startEntry + 1} to ${finishEntry} of ${totalEntries} results`}</div> 
                 <div className="flex flex-row">
-
+                    <div>Previous</div>
+                    {
+                        (totalPages === 1) ?
+                        <div>1</div>
+                        : ""
+                    }
+                    {
+                        (totalPages === 2) ?
+                        <div className="flex flex-row">
+                            <div>1</div>
+                            <div>2</div>
+                        </div>
+                        : ""
+                    }
+                    {
+                        (totalPages === 3) ?
+                        <div className="flex flex-row">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                        </div>
+                        : ""
+                    }
+                    {
+                        (totalPages === 4) ?
+                        <div className="flex flex-row">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                        </div>
+                        : ""
+                    }
+                    {
+                        (totalPages === 5) ?
+                        <div className="flex flex-row">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                            <div>5</div>
+                        </div>
+                        : ""
+                    }
+                    {
+                        (totalPages > 5) ?
+                        <div className="flex flex-row">
+                            {
+                                (currentPage === 0) ?
+                                <div>{currentPage + 1}</div>
+                                : <div>1</div>
+                            }
+                            <div>{currentPage + 2}</div>
+                            <div>{currentPage + 3}</div>
+                            <div>{totalPages}</div>
+                        </div>
+                        : ""
+                    }
+                    <div>Next</div>
                 </div>
             </div>
         </div>
@@ -25,3 +83,6 @@ function PaginationBar(props) {
 }
 
 export default PaginationBar;
+/*
+
+*/
