d4f4c40ef5b2f138b7f668e567d5bb17ef2d77ab
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-02-10 12:45:59 -0700

    account info

--- src/components/IndividualAccount.js
+++ src/components/IndividualAccount.js
@@ -1,13 +1,133 @@
 import React from "react";
+import MoneyIcon from "../components/MoneyIcon.js";
+import PriceTagIcon from "../components/PriceTagIcon.js";
+import DepositIcon from "../components/DepositIcon.js";
+import TransactionIcon from "../components/TransactionIcon.js";
 
 function IndividualAccount(props) {
     const { 
         individualAccount,
         categoriesAndItems
     } = props;
+
+    const getAccountTypeName = () => {
+        if(individualAccount.accountTypeId === 1) {
+            return "Checking";
+        }
+        if(individualAccount.accountTypeId === 2) {
+            return "Savings";
+        }
+        if(individualAccount.accountTypeId === 3) {
+            return "Debt"
+        }
+    }
+
+    const accountTypeName = getAccountTypeName();
+    
     return(
         <div>    
-            {individualAccount.accountName}
+            <div className="flex flex-column w-100 items-center">
+                <div 
+                    className="
+                        w-100 flex justify-center items-center bg-custom-lighter-gray money-color border-thin-gray 
+                        h2 f4 pt1
+                        h2-s f4-s
+                        h2-ns f4-ns
+                        h3-m f3-m
+                        h3-l f3-l
+                        "
+                    >{individualAccount.accountName} - {accountTypeName}
+                </div>
+                <div 
+                className="w-100 flex flex-column 
+                flex-s flex-column-s
+                flex-m flex-column-m
+                flex-l flex-row-l ph2-l
+                "
+                >
+                    <div className="
+                    w-90 flex flex-column bg-custom-lighter-gray mt2
+                    mh1-l
+                    "
+                    >
+                        <div className="flex flex-row items-center mt1 mb1 pl3">
+                            <div 
+                            className="
+                            w2 h2
+                            ">
+                                <MoneyIcon />
+                            </div>
+                            <div className="f4 custom-gray b">Current Balance</div>
+                        </div>
+                        <div className="
+                        f3 money-color flex items-center mb1 pl3
+                        ">   
+                            ${individualAccount.currentBalance}
+                        </div>
+                    </div>
+                    <div className="
+                    w-90 flex flex-column bg-custom-lighter-gray mt2
+                    mh1-l
+                    "
+                    >
+                        <div className="flex flex-row items-center mt1 mb1 pl3">
+                            <div 
+                            className="
+                            w2 h2
+                            
+                            ">
+                                <PriceTagIcon />
+                            </div>
+                            <div className="f4 custom-gray b">Monthly Spending</div>
+                        </div>
+                        <div className="
+                        f3 red flex items-center mb1 pl3
+                        ">   
+                            ${individualAccount.currentBalance}
+                        </div>
+                    </div>
+                    <div className="
+                    w-90 flex flex-column bg-custom-lighter-gray mt2
+                    mh1-l
+                    "
+                    >
+                        <div className="flex flex-row items-center mt1 mb1 pl3">
+                            <div 
+                            className="
+                            w2 h2
+                            ">
+                                <DepositIcon />
+                            </div>
+                            <div className="f4 custom-gray b">Monthly Deposits</div>
+                        </div>
+                        <div className="
+                        f3 money-color flex items-center mb1 pl3
+                        ">   
+                            ${individualAccount.currentBalance}
+                        </div>
+                    </div>
+                    <div className="
+                    w-90 flex flex-column bg-custom-lighter-gray mt2
+                    mh1-l
+                    "
+                    >
+                        <div className="flex flex-row items-center mt1 mb1 pl3">
+                            <div 
+                            className="
+                            w2 h2
+                            ">
+                                <TransactionIcon />
+                            </div>
+                            <div className="f4 custom-gray b">Monthly Transactions</div>
+                        </div>
+                        <div className="
+                        f3 yellow flex items-center mb1 pl3
+                        ">   
+                            {individualAccount.transactions.length}
+                        </div>
+                    </div>  
+                </div>
+            </div>
         </div>
     )
 }
