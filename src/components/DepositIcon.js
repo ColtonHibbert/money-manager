79d4ec726677a4db756cc357d4f26964f03ba971
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-02-11 15:09:28 -0700

    css adjust svg attributes

--- src/components/DepositIcon.js
+++ src/components/DepositIcon.js
@@ -5,14 +5,14 @@ function DepositIcon() {
         <div>
             <svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	        viewBox="0 0 32 32" space="preserve">
-            <path class="deposit0" d="M28,26H4c-1.1,0-2-0.9-2-2V6c0-1.1,0.9-2,2-2h24c1.1,0,2,0.9,2,2v18C30,25.1,29.1,26,28,26z"/>
-            <path class="deposit0" d="M25,22H7c-0.6,0-1-0.4-1-1V9c0-0.6,0.4-1,1-1h18c0.6,0,1,0.4,1,1v12C26,21.6,25.6,22,25,22z"/>
-            <circle class="deposit0" cx="19" cy="15" r="4"/>
-            <line class="deposit0" x1="17" y1="15" x2="13" y2="15"/>
-            <line class="deposit0" x1="5" y1="11" x2="7" y2="11"/>
-            <line class="deposit0" x1="5" y1="19" x2="7" y2="19"/>
-            <polygon class="deposit0" points="8,26 4,26 4,29 7,29 "/>
-            <polygon class="deposit0" points="24,26 28,26 28,29 25,29 "/>
+            <path className="deposit0" d="M28,26H4c-1.1,0-2-0.9-2-2V6c0-1.1,0.9-2,2-2h24c1.1,0,2,0.9,2,2v18C30,25.1,29.1,26,28,26z"/>
+            <path className="deposit0" d="M25,22H7c-0.6,0-1-0.4-1-1V9c0-0.6,0.4-1,1-1h18c0.6,0,1,0.4,1,1v12C26,21.6,25.6,22,25,22z"/>
+            <circle className="deposit0" cx="19" cy="15" r="4"/>
+            <line className="deposit0" x1="17" y1="15" x2="13" y2="15"/>
+            <line className="deposit0" x1="5" y1="11" x2="7" y2="11"/>
+            <line className="deposit0" x1="5" y1="19" x2="7" y2="19"/>
+            <polygon className="deposit0" points="8,26 4,26 4,29 7,29 "/>
+            <polygon className="deposit0" points="24,26 28,26 28,29 25,29 "/>
             </svg>
         </div>
     )
