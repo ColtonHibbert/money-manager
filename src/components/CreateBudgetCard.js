f8b4d8aef0a1a6dcda656638e0fe4ff8a5650b39
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2021-04-07 15:55:26 -0600

    budget card

---
+++ src/components/CreateBudgetCard.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+function CreateBudgetCard(props) {
+
+    const {
+        categoriesAndItems
+    } = props;
+
+
+    return(
+        <div className="w-90 flex flex-column bg-custom-lighter-gray
+        w-90-m flex-column-m
+        w-70-l flex-row-l
+        ">
+            <div className="flex-flex-column">
+                <div className="flex-flex-column">
+                    <div className="ml3 f4 custom-gray mt2">
+                        Create Budget Categories
+                    </div>
+                    
+                    <div className="flex flex-column mv2">
+                        <div className="pl3 f5 custom-gray">
+                            Category Name:
+                        </div>
+                        <input type="text" 
+                        className="h2 input-reset mh3 bg-custom-lighter-gray custom-gray border-custom-gray form-line-active b
+                    
+                        "
+                        //placeholder={user.lastName}
+                        //onInput={(event) => setProfileLastName(event.target.value)}
+                        //value={}
+                        >
+                        </input>
+                    </div>
+                    <div className="flex flex-column mv2">
+                        <div className="pl3 f5 custom-gray mt2">
+                            Budget Amount:
+                        </div>
+                        <input type="text" 
+                        className="h2 input-reset mh3 bg-custom-lighter-gray custom-gray border-custom-gray form-line-active b
+                        "
+                        //placeholder={user.lastName}
+                        //onInput={(event) => setProfileLastName(event.target.value)}
+                        //value={}
+                        >
+                        </input>
+                    </div>  
+                    <div className="w4 flex justify-center items-center f5 mv3 mh3 pa1 bg-money-color white br1 pointer grow"
+                    >
+                        Create Category
+                    </div>
+                </div>
+                <div className="flex-flex-column">
+                    <div className="ml3 f4 custom-gray">
+                        Create Items in a Category
+                    </div>
+                    <div className="flex flex-column mv2">
+                        <div className="pl3 f5 custom-gray mt2">
+                            Select Category:
+                        </div>
+                    {
+                        (Array.isArray(categoriesAndItems)) ?
+                        <select className="h2 flex mh3 pl1 input-reset bg-custom-lighter-gray custom-gray border-custom-gray form-line-active b bw1"
+                        //onChange={(event) => handleSelectCategoryAndItem(event.target.value)}
+                        //value={individualAccount.addTransactionPersonalBudgetCategoryItemId !== 0 ? console.log() : ""}
+                        >
+                            <option value="">--</option>
+                            {
+                                categoriesAndItems.map(category => {
+                                    return(
+                                        <option key={category.personalBudgetCategoryId} value={JSON.stringify(category)}>{category.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                        : ""
+                    }
+                    </div>
+                    <div className="flex flex-column mv2">
+                        <div className="pl3 f5 custom-gray">
+                            Item Name:
+                        </div>
+                        <input type="text" 
+                        className="h2 input-reset mh3 bg-custom-lighter-gray custom-gray border-custom-gray form-line-active b
+                        "
+                        //placeholder={user.lastName}
+                        //onInput={(event) => setProfileLastName(event.target.value)}
+                        //value={}
+                        >
+                        </input>
+                        <div className="w4 flex justify-center items-center f5 mv3 mh3 pa1 bg-money-color white br1 pointer grow"
+                    >
+                        Create Item
+                    </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    )
+}
+
+export default CreateBudgetCard;
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,9 +1,30 @@
 import React from "react";
+import CreateBudgetButton from "../components/CreateBudgetButton.js";
+import CreateBudgetCard from "../components/CreateBudgetCard.js";
 
 function Dashboard(props) {
     return (
-        <div>
-            Dashboard
+        <div className="flex flex-column w-100 items-center bg-custom-darker-gray">
+            <div 
+                className="
+                    w-100 flex justify-center items-center bg-custom-lighter-gray money-color border-thin-gray 
+                    h2 f4 pt1
+                    h2-s f4-s
+                    h2-ns f4-ns
+                    h3-m f3-m
+                    h3-l f3-l
+                    "
+                >Dashboard
+            </div>
+            <div className="w-100 flex justify-start">
+                <CreateBudgetButton />
+            </div>
+            <div className="w-100 pl3 mt3">
+                <CreateBudgetCard categoriesAndItems={props.categoriesAndItems} />
+            </div>
+            
+            
+
         </div>
     )
 }
